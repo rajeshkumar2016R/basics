e41da7c34ed82d80ee127afa133df4a5d955dc9e
rajeshkumar2016R <rrrajeshkumarojha2016@gmail.com>
2024-02-07 21:23:09 +0530

    add component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import SubComponents from "./components/SubComponents";
 
 function App() {
+  
   return (
+  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SubComponents />
+        <h1>Hello world !</h1> 
     </div>
   );
 }
---
+++ src/components/SubComponents.js
@@ -0,0 +1,9 @@
+import './SubComponents.css';
+
+function SubComponents(){
+    return (
+        <h1 className="h1">component created...</h1>
+    );
+}
+
+export default SubComponents;
